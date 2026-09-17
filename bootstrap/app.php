6cfc96a736c720ba90d54723e22e42982d8da731
João Andrade <jpjoaopedroandrade@gmail.com>
2026-09-17 17:43:35 +0100

    proxis

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'terminar.sessao' => \App\Http\Middleware\TerminarSessao::class,
         ]);
